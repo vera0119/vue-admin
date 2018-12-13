f890bf068a60f260fba34488d17fac2e21b03ade
vera0119 <vera0119@qq.com>
2018-12-13 09:41:20 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@
 import '@/styles/main.scss'
 
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI, { size: 'medium' })
 
 /* eslint-disable no-new */
 new Vue({
